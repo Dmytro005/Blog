3abc83920849c78cf0583d0b48d30190683ee3c3
email <harazdovskiy@gmail.com>
2017-10-14 11:40:18 +0300

    Directives
    -Improve directive structure

--- public/js/app.js
+++ public/js/app.js
@@ -37,19 +37,34 @@ app.controller("myCtrl", function ($scope) {
 //Connect directive
 
 //Menu html doc connect
+
+app.directive("pages", function () {
+    return {
+        replace: true,
+        templateUrl: "template/pages/pages.html",
+        controller: function () {
+
+
+
+        }
+    }
+});
+
+
+
+
 app.directive("header", function () {
     return {
         replace: true,
         templateUrl: "template/menu.html",
-        controller: function($scope){
+        controller: function ($scope) {
             $scope.home = true;
             $scope.blog = false;
             $scope.contact = false;
 
-            $scope.menuButtons =[
-                {
+            $scope.menuButtons = [{
                     name: "Home",
-                    action: function(){
+                    action: function () {
                         $scope.home = true;
                         $scope.blog = false;
                         $scope.contact = false;
@@ -57,7 +72,7 @@ app.directive("header", function () {
                 },
                 {
                     name: "Blog",
-                    action: function(){
+                    action: function () {
                         $scope.home = false;
                         $scope.blog = true;
                         $scope.contact = false;
@@ -65,7 +80,7 @@ app.directive("header", function () {
                 },
                 {
                     name: "Contact",
-                    action: function(){
+                    action: function () {
                         $scope.home = false;
                         $scope.blog = false;
                         $scope.contact = true;
@@ -106,6 +121,173 @@ app.directive("contactPage", function () {
 app.directive("slider", function () {
     return {
         replace: true,
-        templateUrl: "template/slider/slider.html"
+        templateUrl: "template/slider.html",
+        controller: function () {
+            $(function () {
+                var slideDelay = 4500;
+                var animDuration = 1800;
+
+                var currentSlide = 1;
+                var width = $("#slider").width();
+
+                var $slider = $("#slider");
+                var $sliderContainer = $slider.find("#sliderContainer");
+                var $slides = $sliderContainer.find(".slide");
+                var lastSlide = $slides.length;
+
+                var circleHTML = "";
+                var currentCircle;
+                var previousCircle;
+
+                var interval; // contains our interval playing
+
+                //---------------create circles    
+                for (var i = currentSlide; i <= lastSlide; i++) {
+                    circleHTML += '<div class="circle" slide-id="' + i + '"></div>';
+                }
+                //---------------paste circles    
+                $slider.append("<div class='bar'>" + circleHTML + "</div>");
+
+                //---------------check and activate circles
+                function circleActivate() {
+                    if (previousCircle !== undefined) {
+                        previousCircle.removeClass("active");
+                    }
+                    currentCircle = $(".circle[slide-id='" + currentSlide + "']");
+                    currentCircle.addClass("active");
+                    previousCircle = currentCircle;
+                }
+
+                //---------Upload Photo when it's need
+                function photoUpload() {
+                    let count = currentSlide + 1;
+                    $(".slide:nth-child(" + count + ")").css("background", "url(img/slides/" + count + ".jpg)");
+                }
+                //uppload first photo
+                $(".slide:nth-child(" + (currentSlide) + ")").css("background", "url(img/slides/" + (currentSlide) + ".jpg)");
+
+                //-----scroll to the current slide
+                function scrollToCurrent() {
+                    $("#sliderContainer").animate({ //scroll previous
+                        scrollLeft: width * currentSlide
+                    }, 700);
+                }
+
+                //-----return slidet to first slide
+                function toFirstSlide() {
+                    if (currentSlide === lastSlide + 1) {
+                        currentSlide = 1;
+                        $("#sliderContainer").animate({
+                            scrollLeft: 0
+                        }, 1000);
+                    }
+                }
+
+                //------play slider
+                function play() {
+
+                    circleActivate();
+
+                    interval = setInterval(function () {
+                        nextSlide();
+                        //            console.log(currentSlide);
+                    }, slideDelay)
+                }
+
+                //------stop slider
+                function stop() {
+                    clearInterval(interval);
+                }
+
+                function nextSlide() {
+                    stop();
+
+                    photoUpload();
+
+                    ++currentSlide;
+
+                    circleActivate();
+                    $("#sliderContainer").animate({ //scroll previous
+                        scrollLeft: "+=" + width
+                    }, animDuration);
+
+                    if (currentSlide > lastSlide) {
+                        toFirstSlide();
+                    }
+                    play();
+                    // console.log(currentSlide);
+                }
+
+                function prevSlide() {
+                    stop();
+
+                    --currentSlide;
+
+                    circleActivate();
+
+                    $("#sliderContainer").animate({ //scroll to next
+                        scrollLeft: "-=" + width
+                    }, animDuration);
+
+                    if (currentSlide < 1) {
+                        currentSlide = 1;
+                        circleActivate();
+                    }
+                    play();
+                    console.log(currentSlide);
+
+                }
+
+                //----circle click event handler
+                $(".circle").click(function () {
+
+                    stop(); //stop slider
+
+                    $("#sliderContainer").animate({ //scroll to certain block
+                        scrollLeft: width * ($(this).attr("slide-id") - 1)
+                    }, animDuration, "swing");
+
+                    currentSlide = $(this).attr("slide-id"); //change current slide
+
+                    photoUpload();
+
+                    circleActivate(); //activate properly circle
+
+                    play(); //play slider again
+                })
+
+                //----buttons previous and next event handler
+                $(".btn").click(function () {
+                    if ($(this).attr("id") == "next") {
+                        nextSlide();
+                    } else {
+                        prevSlide();
+                    }
+                });
+
+                //----key work;
+                $("body").keydown(function (e) {
+                    if (e.keyCode == 37) { // left
+                        prevSlide();
+                    } else if (e.keyCode == 39) { // right
+                        nextSlide();
+                    }
+                });
+
+                //    $("#slider").mouseenter(function () {
+                //            stop();
+                //            console.log("enter");
+                //        })
+                //        .mouseleave(function () {
+                //            console.log("leave");
+                //            play();
+                //        });
+
+                //-----activate autoplay
+                play();
+
+            });
+
+        }
     }
 });
